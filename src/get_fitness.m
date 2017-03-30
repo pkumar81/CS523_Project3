470e92961c4f1c13b8f7273b797b369f08dcf974
MengqinCai <mcai1988@unm.edu>
2017-03-30 10:39:29 -0600

    add default input for fitness function

--- src/get_fitness.m
+++ src/get_fitness.m
@@ -1,10 +1,10 @@
 function fitness =  get_fitness(genome_value,index)
 % This is a temp function for testing
     if index=1
-    fitness = bimass_no_image(genome_value);
+    fitness = bimass_no_image(genome_value,0,0);
     end
     if index=2
-    fitness = longevity_no_image(genome_value);
+    fitness = longevity_no_image(genome_value,0,0);
     end
 end
 
