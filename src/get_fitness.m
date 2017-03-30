388774f5c3a8ba76753a3a4941bb760d6151c255
MengqinCai <mcai1988@unm.edu>
2017-03-30 10:58:56 -0600

    fitness calculating for p1 and p2

--- src/get_fitness.m
+++ src/get_fitness.m
@@ -1,10 +1,17 @@
-function fitness =  get_fitness(genome_value,index)
-% This is a temp function for testing
+%index branch get_fitness function to biomass or longevity, if index=1, use biomass fitness function, if index=2, use longevity fitness function
+%genome[] is a array which contains the genome value (p) from one generation
+%fitness[] is a array which contains the fitness for one generation 
+function fitness[] =  get_fitness(genome1[],genome2[],population_size,index)
+%method using p1 and p2 in the same forest, return the same fitness for both p1 and p2
     if index=1
-    fitness = bimass_no_image(genome_value,0,0);
+       for i=1:population_size
+           fitness(i) = bimass_no_image(genome1(i),genome2(i),0);
+       end
     end
     if index=2
-    fitness = longevity_no_image(genome_value,0,0);
-    end
+       for i=1:population_size
+           fitness(i) = longevity_no_image(genome1(i),genome2(i),0);
+       end
+    end 
 end
 
